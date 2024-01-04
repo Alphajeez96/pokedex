af22123904a1cfe168b301038f25300bc334669a
alphajeez <chimeremezechukwudire@gmail.com>
2024-01-04 22:40:16 +0100

    side bar

--- src/views/Pokedex.vue
+++ src/views/Pokedex.vue
@@ -1,9 +1,15 @@
 <template>
-  <main>dkjdk</main>
+  <main>
+    <SideBar />
+  </main>
 </template>
+
+<script lang="ts" setup>
+import SideBar from '../components/partials/SideBar.vue'
+</script>
 
 <style lang="scss" scoped>
 main {
-  @apply bg-black h-screen;
+  @apply bg-black h-screen p-9;
 }
 </style>
