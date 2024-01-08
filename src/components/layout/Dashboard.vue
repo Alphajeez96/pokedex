468b3b2f4436e109db6154b382bbf89cdf9b3d7b
alphajeez <chimeremezechukwudire@gmail.com>
2024-01-08 04:47:49 +0100

    vector animation

--- src/components/layout/Dashboard.vue
+++ src/components/layout/Dashboard.vue
@@ -7,17 +7,9 @@
     </div>
 
     <aside>
-      <img
-        class="absolute bottom-0 left-0"
-        src="../../assets/ash.svg"
-        alt="ash"
-      />
+      <img class="ash" src="../../assets/ash.svg" alt="ash" />
 
-      <img
-        class="absolute bottom-0 right-0"
-        src="../../assets/pikachu.svg"
-        alt="pikachu"
-      />
+      <img class="pikachu" src="../../assets/pikachu.svg" alt="pikachu" />
     </aside>
   </main>
 </template>
@@ -29,5 +21,40 @@ import SideBar from '../partials/SideBar.vue'
 <style lang="scss" scoped>
 main {
   @apply bg-black h-screen p-9 flex gap-9;
+}
+
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+
+.ash {
+  @apply absolute bottom-0 left-0;
+  animation: fadeIn 2s ease-out;
+}
+
+@keyframes popAndBounce {
+  0% {
+    transform: translateY(100%) scale(0);
+    opacity: 0;
+  }
+  60% {
+    transform: translateY(-20%) scale(1.2);
+  }
+  80% {
+    transform: translateY(10%);
+  }
+  100% {
+    transform: translateY(0) scale(1);
+  }
+}
+
+.pikachu {
+  @apply absolute bottom-0 right-0;
+  animation: popAndBounce 1s ease-out;
 }
 </style>
